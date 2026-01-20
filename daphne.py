ae5546152c422510a86f78483973e4689a11939c
MoneyMindsetMakeover <info@moneymindsetmakeover.org>
2026-01-20 10:07:03 -0500

    Update daphne.py

--- daphne.py
+++ daphne.py
@@ -1,1 +1,15 @@
+import streamlit as st
+import pandas as pd
+from utils import load_daphne_data
 
+def get_daphne_status():
+    """Return DAPHNE agent status"""
+    return "Active"
+
+def get_daphne_leads():
+    """Get DAPHNE donor prospects as list of dictionaries"""
+    try:
+        df = load_daphne_data()
+        return df.to_dict('records') if not df.empty else []
+    except:
+        return []
